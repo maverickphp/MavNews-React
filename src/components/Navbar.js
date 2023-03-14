096f5330738f8dba9f1913fae26cd45e0dce7134
Fasih Ul Haq <fasih682@gmail.com>
2023-03-14 16:58:45 +0500

    Add clsses / props / this state

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,7 @@
-import PropTypes from 'prop-types'
 import React, { Component } from 'react'
 
 export class Navbar extends Component {
-  static propTypes = {}
+
 
   render() {
     return (
--- src/components/News.js
+++ src/components/News.js
@@ -4,15 +4,21 @@ import NewsItem from './NewsItem'
 export class News extends Component {
   render() {
     return (
-      <div>
-        News Component
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
+      <div className='container my-3'>
+        <h2>Top Headlines</h2> 
+        <div className= "row">
+          <div className="col-md-3">
+                <NewsItem title="mytitle" description ="mydesc" imageURL="https://cdn.discordapp.com/attachments/669107164098330654/1084848066202456185/SEO.png" newsURL="ASD"/>
+          </div>
+          <div className="col-md-3">
+                <NewsItem title="mytitle" description ="mydesc" imageURL="https://cdn.discordapp.com/attachments/669107164098330654/1084848066202456185/SEO.png" newsURL="ASD"/>
+          </div>
+          <div className="col-md-3">
+                <NewsItem title="mytitle" description ="mydesc" imageURL="https://cdn.discordapp.com/attachments/669107164098330654/1084848066202456185/SEO.png" newsURL="ASD"/>
+          </div>
+                
+        </div>
+
         </div>
     )
   }
